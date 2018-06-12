33d2c8b229dde17683a7231d1621d1b3de8e1349
Hemesh-Unka <hemeshunka@gmail.com>
2018-06-12 16:08:37 +0100

    added address cli feature

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const request = require('request')
+const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describle: 'Address to fetch weather for',
+      string: true,
+    },
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=%223-4%20holborn%20circus%20London%22&key=AIzaSyBW9Z7I3woiO4vwmm0DQjlnLEMhqgCwJnU',
-  json: true
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${argv.address}&key=AIzaSyBW9Z7I3woiO4vwmm0DQjlnLEMhqgCwJnU`,
+  json: true,
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);
   console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
