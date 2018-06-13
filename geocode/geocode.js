b27a90a6785ab0decf7bf34718cb99b84907ffa1
Hemesh-Unka <hemeshunka@gmail.com>
2018-06-13 12:21:46 +0100

    removed api key from url

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,22 +1,23 @@
+const dotenv = require('dotenv').config();
 const request = require('request');
 
 const geocodeAddress = (address, callback) => {
-    request({
-        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyBW9Z7I3woiO4vwmm0DQjlnLEMhqgCwJnU`,
-        json: true,
-    }, (error, response, body) => {
-        if (error) {
-            callback('Unable to connect to google servers.');
-        } else if (body.status === 'ZERO_RESULTS') {
-            callback('Unable to find that address.');
-        } else if (body.status === 'OK') {
-            callback(undefined, {
-                address: body.results[0].formatted_address,
-                latitude: body.results[0].geometry.location.lat,
-                longitude: body.results[0].geometry.location.lng,
-            });
-        }
-    });
+  request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.GM_API_KEY}`,
+    json: true,
+  }, (error, response, body) => {
+    if (error) {
+      callback('Unable to connect to google servers.');
+    } else if (body.status === 'ZERO_RESULTS') {
+      callback('Unable to find that address.');
+    } else if (body.status === 'OK') {
+      callback(undefined, {
+        address: body.results[0].formatted_address,
+        latitude: body.results[0].geometry.location.lat,
+        longitude: body.results[0].geometry.location.lng,
+      });
+    }
+  });
 };
 
 module.exports.geocodeAddress = geocodeAddress;
