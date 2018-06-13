5b31079bad3ce74e906a5c3211eab409a52ce6f9
Hemesh-Unka <hemeshunka@gmail.com>
2018-06-13 12:26:14 +0100

    updated indentation

--- app.js
+++ app.js
@@ -4,29 +4,29 @@ const geocode = require('./geocode/geocode.js');
 const weather = require('./weather/weather.js');
 
 const argv = yargs
-    .options({
-        a: {
-            demand: true,
-            alias: 'address',
-            describle: 'Address to fetch weather for',
-            string: true,
-        },
-    })
-    .help()
-    .alias('help', 'h')
-    .argv;
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describle: 'Address to fetch weather for',
+      string: true,
+    },
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
 
 geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-    if (errorMessage) {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(results.address);
+    weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+      if (errorMessage) {
         console.log(errorMessage);
-    } else {
-        console.log(results.address);
-        weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
-            if (errorMessage) {
-                console.log(errorMessage);
-            } else {
-                console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}`);
-            }
-        });
-    }
+      } else {
+        console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}`);
+      }
+    });
+  }
 });
